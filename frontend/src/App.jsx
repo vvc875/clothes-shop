778018dfc6fdb3aa8c49112c5b2edb59acfa736d
Nguyen Van Khiem <khiemtrumdausi@gmail.com>
2026-04-18 11:24:27 +0700

    Chỉnh router cho các file khách hàng trong file App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// --- CUSTOMER SCREENS ---
+import HomeScreen from "./screens/customer/HomeScreen";
+import ProductScreen from "./screens/customer/ProductScreen";
+import CartScreen from "./screens/customer/CartScreen";
+import LoginScreen from "./screens/Loginscreen";
+
+// --- ADMIN SCREENS ---
 import DashboardScreen from "./screens/admin/DashboardScreen";
 import ProductListScreen from "./screens/admin/ProductListScreen";
 import UserListScreen from "./screens/admin/UserListScreen";
 import OrderListScreen from "./screens/admin/OrderListScreen";
-import LoginScreen from "./screens/Loginscreen";
 
+// --- COMPONENTS ---
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 
@@ -14,11 +21,27 @@ function App() {
     return (
         <Router>
             <Routes>
-                {/* Trang Login không có Sidebar */}
+                {/* 1. TRANG ĐĂNG NHẬP (Giao diện sạch) */}
                 <Route path="/login" element={<LoginScreen />} />
-                <Route path="/" element={<LoginScreen />} />
 
-                {/* Các trang Admin có Sidebar */}
+                {/* 2. CÁC TRANG KHÁCH HÀNG (Có Header ở trên) */}
+                <Route
+                    path="/*"
+                    element={
+                        <>
+                            <Header />
+                            <main>
+                                <Routes>
+                                    <Route path="/" element={<HomeScreen />} />
+                                    <Route path="/product/:id" element={<ProductScreen />} />
+                                    <Route path="/cart/:id?" element={<CartScreen />} />
+                                </Routes>
+                            </main>
+                        </>
+                    }
+                />
+
+                {/* 3. CÁC TRANG ADMIN (Có Sidebar bên trái + Header) */}
                 <Route
                     path="/admin/*"
                     element={
@@ -32,24 +55,14 @@ function App() {
                                 }}
                             >
                                 <Header />
-                                <Routes>
-                                    <Route
-                                        path="dashboard"
-                                        element={<DashboardScreen />}
-                                    />
-                                    <Route
-                                        path="productlist"
-                                        element={<ProductListScreen />}
-                                    />
-                                    <Route
-                                        path="userlist"
-                                        element={<UserListScreen />}
-                                    />
-                                    <Route
-                                        path="orderlist"
-                                        element={<OrderListScreen />}
-                                    />
-                                </Routes>
+                                <div className="p-4">
+                                    <Routes>
+                                        <Route path="dashboard" element={<DashboardScreen />} />
+                                        <Route path="productlist" element={<ProductListScreen />} />
+                                        <Route path="userlist" element={<UserListScreen />} />
+                                        <Route path="orderlist" element={<OrderListScreen />} />
+                                    </Routes>
+                                </div>
                             </div>
                         </div>
                     }
